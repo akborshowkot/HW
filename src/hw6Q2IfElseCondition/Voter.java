7f9e26c96618893041bf0427a6209ff652cdaa9a
akborshowkot <akbormdshowkot007@gmail.com>
2022-02-13 23:14:52 -0500

    02132022 11.4pm

---
+++ src/hw6Q2IfElseCondition/Voter.java
@@ -0,0 +1,17 @@
+package hw6Q2IfElseCondition;
+
+public class Voter {
+	public static void main(String[] args) {
+		int age = 18;
+		if (age == 18) {
+			System.out.println("I am  a voter");
+		} else if (age < 18) {
+			System.out.println("I am not a voter");
+		} else if (age > 18) {
+			System.out.println("I am a voter");
+		} else {
+			System.out.println("Please add a valid age");
+		}
+
+	}
+}
---
+++ src/hw7Q2UseOfNestedIf/DiabeticCondition.java
@@ -0,0 +1,18 @@
+package hw7Q2UseOfNestedIf;
+import java.util.Scanner;
+public class DiabeticCondition {
+   public static void main(String[] args) {
+		System.out.println("<-- Please Enter your Hemoglobin A1C value below -->");
+Scanner scanner	=new Scanner(System.in);
+double hbga1c= scanner.nextDouble();
+if (hbga1c>6.4) {
+	System.out.println("I am a diabetic patient");
+	
+}
+   
+   
+   }
+   
+   
+
+}
